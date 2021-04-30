a35a29a7511aa0df41f58551257ddef5b6ea3223
Ivan_Polovnikov <i.a.polovnikov@gmail.com>
2021-04-30 21:09:03 +0700

    homework 3 pre

---
+++ spring-mvc/src/main/java/com/ttsr/spring_mvc/controller/ExceptionController.java
@@ -0,0 +1,34 @@
+package com.ttsr.spring_mvc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author antonkuznetsov
+ */
+@ControllerAdvice
+@Slf4j
+public class ExceptionController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public void badRequest() {
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
+        log.error("Request: " + req.getRequestURL() + " raised " + ex);
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+        return mav;
+    }
+}
